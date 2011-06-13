158065240393c2283670533ab447e23c4b893260
liutaihua <defage@gmail.com>
2011-06-13 09:20:41 +0800

    write config to cldisky.conf,when run cldisky_confecho command.

--- cldisky/confecho.py
+++ cldisky/confecho.py
@@ -1,8 +1,11 @@
 import pkg_resources
 import sys
 
+file = open('/etc/cldisky.conf','w')
 def main(out=sys.stdout):
     config = pkg_resources.resource_string(__name__, 'skel/sample.conf')
     out.write(config)
+	file.write(config)
+	file.close()
 
 
