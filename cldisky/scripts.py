636b2f87ea0756dd22261c0bc0b733cfbda7bd40
liutaihua <defage@gmail.com>
2011-07-07 18:12:12 +0800

    fix error

--- cldisky/scripts.py
+++ cldisky/scripts.py
@@ -5,6 +5,9 @@ import sys
 from scaning import MyDaemon
 
 def cldisky():
+    if not os.path.exists('/etc/cldisky.conf'):
+        os.system("cldisky_confecho")
+        sys.exit(0)
     if not sys.argv[1:]:
         print "commands: start|stop|restart"
         sys.exit(0)
@@ -19,13 +22,5 @@ def cldisky():
         daemon.start()
     else:
         print "commands: start|stop|restart"
-
-def confecho(out=sys.stdout):
-    config = pkg_resources.resource_string(__name__, 'skel/sample.conf')
-    out.write(config)
-    if not os.path.exists('/etc/cldisky.conf'):
-        file = open('/etc/cldisky.conf','w')
-        file.write(config)
-        file.close()
 #if __name__ == "__main__":
 #    confecho()
